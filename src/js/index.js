4d74df7ab45f689c821896304961e710df6d0c62
GaliyaZ <galiya753@gmail.com>
2020-12-01 12:44:54 +0500

    sdd dev-server, copy-webpack-plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
-import Post from './post';
-import json from '../assets/json.json';
+import Post from '@models/post';
+// import json from '../assets/json.json';
 import css from '../styles/styles.css';
-import myPicture from '../assets/after.png';
+import myPicture from '@/assets/after.png';
+// import xml from '../assets/xml.xml'
+// import csv from '../assets/csv.csv'
+import * as $ from 'jquery'
 
 const post = new Post('Webpack post title', myPicture);
 
-console.log('post to string: ', post.toString());
-console.log('JSON: ', json)
+$('pre').addClass('code').html(post.toString());
+
+//console.log('post to string: ', post.toString());
+// console.log('JSON: ', json);
+// console.log('XML: ', xml);
+// console.log('CSV: ', csv);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'), 
@@ -13,17 +14,41 @@ module.exports = {
     filename: '[name].[contenthash].js', //webpack result
     path: path.resolve(__dirname, 'dist') //result path (from current dir)
   },
+  resolve: {
+    extensions: ['.js', '.json', '.png'], //extensions webpack 
+    alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'}
+  },
+  devServer: {
+    port: 4200
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]}
+    )
   ],
   module: {
     rules: [
       {test: /\.css$/, use: ['style-loader', 'css-loader']},
       {test: /\.(png|jpg|svg|gif|webp)$/, use: ['file-loader']},
-      {test: /\.(woff|woff2|ttf)$/, use: ['file-loader']}
+      {test: /\.(woff|woff2|ttf)$/, use: ['file-loader']},
+      {test: /\.xml$/, use: ['xml-loader']},
+      {test: /\.csv$/, use: ['csv-loader']}
   ]
   }
 }
